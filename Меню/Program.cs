f3d4c479cade5ed1f1fabead9f6d4937f60cbebd
Егор Марков <rupor.222@yandex.ru>
2022-03-29 17:44:17 +0300

    Adding function

--- Меню/Program.cs
+++ Меню/Program.cs
@@ -6,9 +6,10 @@ namespace Меню // Note: actual namespace depends on the project name.
     enum GameMenu
     {
         Начать = 1,
-        Загрузить = 2,
-        Настройки = 3,
-        Выход = 4
+        Начать_новую = 2,
+        Загрузить = 3,
+        Настройки = 4,
+        Выход = 5
     }
     internal class Program 
     {
@@ -20,6 +21,7 @@ namespace Меню // Note: actual namespace depends on the project name.
             Console.WriteLine($"Нажмите {(int)GameMenu.Загрузить} что бы загрузить сохранение");
             Console.WriteLine($"Нажмите {(int)GameMenu.Настройки} что бы открыть настройки");
             Console.WriteLine($"Нажмите {(int)GameMenu.Выход} что бы выйти");
+       
             string input = Console.ReadLine();
             GameMenu menu= (GameMenu)int.Parse(input);
             Console.WriteLine($"Вы выбрали {menu}");
